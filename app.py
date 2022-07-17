913bc02548403edd1e2a1472b5d7b3b2dcc92cb1
Rojen003 <73420085+Rojen003@users.noreply.github.com>
2022-07-17 13:22:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,18 +14,19 @@ def upload_file():
         file = request.files['file']
         print("File Received")
         filename = secure_filename(file.filename)
-        file.save(app.config("static/"+ filename)) #Heroku no need static
-        file = open(app.config("static/"+ filename,"r")) #Heroku no need static
+        print(filename)
+        file.save("./static/"+filename) #Heroku no need static
+        file = open("./static/"+filename,"r") #Heroku no need static
         model = load_model("Pneumonia")
-        img = Image.open(filename) #rose = 3, sunflower = 4, tulip 5
-        img = img.resize((100,100))
+        image = cv2.imread("./static/"+filename)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        img = cv2.merge([gray,gray,gray])
+        img.resize((150,150,3))
         img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
         img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
-        img.shape
         pred=model.predict(img)
-        return(render_template("index1.html", result=str(pred)))
+        return(render_template("index.html", result=str(pred)))
     else:
-        return(render_template("index1.html", result="2"))
-    
+        return(render_template("index.html", result="WAITING"))
 if __name__ == "__main__":
     app.run()
