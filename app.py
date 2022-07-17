9211e048a4c83a878c8f513d5be21f9e8669bc69
Rojen003 <73420085+Rojen003@users.noreply.github.com>
2022-07-17 13:48:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from skimage import io
 from keras.models import load_model
-import cv2
+#import cv2
 from PIL import Image #use PIL
 import numpy as np
 
@@ -11,21 +11,23 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
-        print("File Received")
-        filename = secure_filename(file.filename)
-        file.save(app.config("static/"+ filename)) #Heroku no need static
-        file = open(app.config("static/" + filename,"r")) #Heroku no need static
-        model = load_model("Pneumonia")
-        img = Image.open(filename) #rose = 3, sunflower = 4, tulip 5
-        img = img.resize((100,100))
-        img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
-        img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
-        img.shape
-        pred=model.predict(img)
-        return(render_template("index1.html", result=str(pred)))
+#         file = request.files['file']
+#         print("File Received")
+#         filename = secure_filename(file.filename)
+#         print(filename)
+#         file.save(filename) #Heroku no need static
+#         file = open(filename,"r") #Heroku no need static
+#         model = load_model("Pneumonia")
+#         image = cv2.imread(filename)
+#         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#         img = cv2.merge([gray,gray,gray])
+#         img.resize((150,150,3))
+#         img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
+#         img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
+#         pred=model.predict(img)
+        pred = "YES"
+        return(render_template("index.html", result=str(pred)))
     else:
-        return(render_template("index1.html", result="2"))
-    
+        return(render_template("index.html", result="WAITING"))
 if __name__ == "__main__":
     app.run()
